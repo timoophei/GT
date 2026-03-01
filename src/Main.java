e05106c899deee16174315926c446d518747547f
Tsimafei Heiba <timopheiheiba@gmail.com>
2026-03-01 16:24:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         System.out.println("Hello World");
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
-            System.out.println(i + "123");
         }
     }
 }
